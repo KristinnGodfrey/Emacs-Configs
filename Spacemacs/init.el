6b1660dab07585ca16e7b04f03395777cd9e01e9
Kristinn Godfrey <kristinngodfrey@gmail.com>
2021-05-14 01:17:43 +0000

    Update init.el

--- Spacemacs/init.el
+++ Spacemacs/init.el
@@ -1,4 +1,7 @@
-;;; init.el --- Spacemacs Initialization File
+;;; init.el --- Initialization file for Emacs
+;;; package --- Summary
+;;; Code:
+;;; Commentary:
 ;;
 ;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
 ;;
@@ -13,40 +16,33 @@
 ;; (package-initialize)
 
 ;; Increase gc-cons-threshold, depending on your system you may set it back to a
-;; lower value in your dotfile (function `dotspacemacs/user-config')
+;;lower value in your dotfile (function `dotspacemacs/user-config')
 (setq gc-cons-threshold 100000000)
 
 (defconst spacemacs-version         "0.200.13" "Spacemacs version.")
 (defconst spacemacs-emacs-min-version   "24.4" "Minimal version of Emacs.")
 
-(if (not (version<= spacemacs-emacs-min-version emacs-version))
-    (error (concat "Your version of Emacs (%s) is too old. "
-                   "Spacemacs requires Emacs version %s or above.")
-           emacs-version spacemacs-emacs-min-version)
-  (load-file (concat (file-name-directory load-file-name)
-                     "core/core-load-paths.el"))
-  (require 'core-spacemacs)
-  (spacemacs/init)
-  (configuration-layer/sync)
-  (spacemacs-buffer/display-startup-note)
-  (spacemacs/setup-startup-hook)
-  (require 'server)
-  (unless (server-running-p) (server-start)))
+;; (if (not (version<= spacemacs-emacs-min-version emacs-version))
+;;     (error (concat "Your version of Emacs (%s) is too old. "
+;;                    "Spacemacs requires Emacs version %s or above.")
+;;            emacs-version spacemacs-emacs-min-version)
+;;   (load-file (concat (file-name-directory load-file-name)
+;;                      "core/core-load-paths.el"))
+;;   (require 'core-spacemacs)
+;;   (spacemacs/init)
+;;   (configuration-layer/sync)
+;;   (spacemacs-buffer/display-startup-note)
+;;   (spacemacs/setup-startup-hook)
+;;   (require 'server)
+;;   (unless (server-running-p) (server-start)))
 
 
 (global-set-key (kbd "C-u") 'undo)
-;;C-r search-backward
 (global-set-key (kbd "C-r") 'redo)
-;;C-s search-forward
 (global-set-key (kbd "C-x s") 'save-buffer)
 (global-set-key (kbd "C-x u") 'undo-tree-visualize)
-
 (global-set-key (kbd "C-s") 'isearch-forward)
-
-
-;;"M-e") 'forward-sentence)
 (global-set-key (kbd "M-e") 'end-of-buffer)
-;;"M-e") 'backward-sentence)
 (global-set-key (kbd "M-a") 'beginning-of-buffer)
 
 (global-set-key (kbd "C-c C-e") 'org-export-dispatch)
@@ -55,38 +51,25 @@
 (eval-after-load "org"
   '(require 'ox-md nil t))
 
-(defun org/get-headline-string-element  (headline backend info)
-  (let ((prop-point (next-property-change 0 headline)))
-    (if prop-point (plist-get (text-properties-at prop-point headline) :parent))))
+;; (defun org/get-headline-string-element  (headline backend info)
+;;   (let ((prop-point (next-property-change 0 headline)))
+;;     (if prop-point (plist-get (text-properties-at prop-point headline) :parent))))
 
-(defun org/ensure-latex-clearpage (headline backend info)
-  (when (org-export-derived-backend-p backend 'latex)
-    (let ((elmnt (org/get-headline-string-element headline backend info)))
-      (when (member "newpage" (org-element-property :tags elmnt))
-        (concat "\\clearpage\n" headline)))))
+;; (defun org/ensure-latex-clearpage (headline backend info)
+;;   (when (org-export-derived-backend-p backend 'latex)
+;;     (let ((elmnt (org/get-headline-string-element headline backend info)))
+;;       (when (member "newpage" (org-element-property :tags elmnt))
+;;         (concat "\\clearpage\n" headline)))))
 
 (setq org-image-actual-width '(300))
-
-
-
-
-
 (setq org-html-link-org-files-as-html nil)
-
-
-
 (setq org-image-actual-width 100)
-
 
 (require 'ox-extra)
 (ox-extras-activate '(ignore-headlines))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;;(package-initialize)
 
 (defun indent-file ()
   "Run org babel codeblock formatting in sequence."
@@ -95,17 +78,18 @@
   (call-interactively 'indent-region)
 	(call-interactively 'goto-last-change))
 
-
 (global-set-key (kbd "C-t") 'my-run-org-babel-codeblock-format)
 (global-set-key (kbd "C-;") 'goto-last-change)
+(global-set-key (kbd "C-p") 'previous-line)
+(global-set-key (kbd "C-n") 'next-line)
+(global-set-key (kbd "C-c C-p") 'org-backward-element)
+(global-set-key (kbd "C-c C-n") 'org-forward-element)
+(global-set-key (kbd "M-}") 'outline-next-visible-heading)
+(global-set-key (kbd "M-{") 'outline-previous-visible-heading)
 
 
 
 (setq org-toggle-inline-image nil)
 
-(defun markdown-convert-buffer-to-org ()
-  "Convert the current buffer's content from markdown to orgmode format and save it with the current buffer's file name but with .org extension."
-  (interactive)
-  (shell-command-on-region (point-min) (point-max)
-                           (format "pandoc -f markdown -t org -o %s"
-                                   (concat (file-name-sans-extension (buffer-file-name)) ".org"))))
+(provide 'init)
+;;; init.el ends here
